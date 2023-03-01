d4524946877929ae965591ba66665763b4df2a56
Arijit Acharya <arijit.acharya@innoraft.com>
2023-03-01 19:25:39 +0530

    FT2023-79: Rectified some of the changes requested in PR.

--- task_1/index.php
+++ task_1/index.php
@@ -14,24 +14,27 @@
   <div class="container">
   <?php
 
-    /**
-    * Calling and using the php guzzle package.
-    */
+    //Calling and using the php guzzle package.
     require_once "vendor/autoload.php";
     use GuzzleHttp\Client;
 
     /**
      * This class is created inorder to fetch data from an api and create a website from it.
+     * @method $getdata
+     * It fetches the api data
+     * @method $storingValue.
+     * It stores and prints the value.
      */
     class FetchData {
 
       /**
        * This method is used for calling an api, storing its json response, decoding the json response and  returning it.
        * Since we are using Gazzle, this function creates an object from the "Client" class of Gazzle, and stores the response of the api with its help.  
-       * @param $url is used for getting the url of api.
+       * @param $url 
+       * It is used for getting the url of api.
        * @return $data is for returning the decoded json data obtained . 
        */
-      function get_data( $url) {
+      function getData(string $url): array {
         $client = new Client();
         $response = $client->get($url);
         $json = $response->getBody()->getContents();
@@ -41,9 +44,10 @@
 
       /**
        * This function is responsible for fetching the values , storing and printing it.
-       * @param $main_response_data is used to collect the json decoded response from where we shall obtain the data.
+       * @param $main_response_data 
+       * It is used to collect the json decoded response from where we shall obtain the data.
        */
-      function storing_value($main_response_data) {
+      function storingValue($main_response_data): void {
 
         // Storing the value of the no of items present in the body of the response.
         $len = count($main_response_data['data']);
@@ -62,12 +66,12 @@
 
           // calling api for getting the image and storing it in $store_image variable.
           $image_get_api = $main_response_data['data'][$i]['relationships']['field_image']['links']['related']['href'];;
-          $image_response_data = $this->get_data($image_get_api);
+          $image_response_data = $this->getData($image_get_api);
           $store_image = $prefix_link .  $image_response_data['data']['attributes']['uri']['url'];
 
           // calling api for getting the explore link and storing it in $store_explore_link variable.
           $explore_link_get_api = $main_response_data['data'][$i]['links']['self']['href'];
-          $explore_link_data = $this->get_data($explore_link_get_api);
+          $explore_link_data = $this->getData($explore_link_get_api);
           $store_explore_link = $prefix_link . $explore_link_data['data']['attributes']['path']['alias'];
 
           // Setting a conditon to check if the body of a particular section is present or not. If not we omit the whole section.
@@ -77,20 +81,20 @@
               <div class="text-section col">
                 <div class="title">
                   <!--Printing the titles of a section.  -->
-                  <h2> <a href="<?php echo $store_explore_link ?>"><?php echo $store_titles ?></a></h2>
+                  <h2> <a href="<?php echo $store_explore_link;?>"><?php echo $store_titles ?></a></h2>
                 </div>
                 <div class="body">
                   <!-- Printing the body of a section. -->
-                  <?php echo $list_items['value'] ?>
+                  <?php echo $list_items['value'];?>
                 </div>
                 <div class="button">
                   <!-- Printing the link of a section. -->
-                  <a href="<?php echo $store_explore_link ?>">EXPLORE MORE</a>
+                  <a href="<?php echo $store_explore_link;?>">EXPLORE MORE</a>
                 </div>
               </div>
               <div class="image-section col">
                 <!-- Printing the image of a section. -->
-                <img src="<?php echo $store_image ?>" alt="image">
+                <img src="<?php echo $store_image;?>" alt="image">
               </div>
             </div> 
             <?php
@@ -100,17 +104,13 @@
 
     }
 
-    /**
-     * Storing the main api's link and passing it.
-     */
+    //Storing the main api's link and passing it.
     $url_main = 'https://ir-dev-d9.innoraft-sites.com/jsonapi/node/services';
 
-    /**
-     * Creating an object of the class FetchData and calling the methods.
-     */
+    //Creating an object of the class FetchData and calling the methods.
     $api = new FetchData;
-    $main_response_data = $api->get_data($url_main);
-    $api->storing_value($main_response_data);
+    $main_response_data = $api->getData($url_main);
+    $api->storingValue($main_response_data);
   ?>
 </body>
 
--- task_2/action.php
+++ task_2/action.php
@@ -23,58 +23,43 @@
   
   if (isset($_POST["submit"])) {
 
-    /**
-     *  This line is used to call the phpmailer and use it.
-     *  @param TRUE is used for throwing exceptions on errors.
-     */
+    
+    //This line is used to call the phpmailer and use it.
+    //@param TRUE is used for throwing exceptions on errors.
     $mail = new PHPMailer(TRUE);
 
-    /**
-     * The function "isSMTP()" is been used when you want to tell PHPMailer class to use, the custom SMTP configuration defined instead of the local mail server.
-     * The Simple Mail Transfer Protocol (SMTP) is an application used by mail servers to send, receive, and relay outgoing email between senders and receivers.
-     */
+
+    //The function "isSMTP()" is been used when you want to tell PHPMailer class to use, the custom SMTP configuration defined instead of the local mail server.
+    //The Simple Mail Transfer Protocol (SMTP) is an application used by mail servers to send, receive, and relay outgoing email between senders and receivers.
+  
     $mail->isSMTP();
     $mail->Host = "smtp.gmail.com";
 
-    /**
-     * It is an extension of the Simple Mail Transfer Protocol (SMTP) whereby a client may log in using any authentication mechanism supported by the server. 
-     * It is mainly used by submission servers, where authentication is mandatory.
-     */
+    
+    //It is an extension of the Simple Mail Transfer Protocol (SMTP) whereby a client may log in using any authentication mechanism supported by the server. 
+    //It is mainly used by submission servers, where authentication is mandatory.
     $mail->SMTPAuth = true;
 
-    /**
-     * It is the user name of the sender.
-     */
+    //It is the user name of the sender.
     $mail->Username = "evalid41@gmail.com";
 
-    /**
-     * Passward generated from gmail by the sender.
-     */
+    // Passward generated from gmail by the sender.
     $mail->Password = "iyljhnsjinsvyvjo";
-
     $mail->SMTPSecure = "ssl";
     $mail->Port = 465;
 
-    /**
-     * Setting the sender's email address.
-     */
+    //Setting the sender's email address.
     $mail->setFrom("evalid41@gmail.com");
 
-    /**
-     * Setting the receiver's email address.
-     */
+    //Setting the receiver's email address.
     $mail->addAddress($_POST["email"]);
     $mail->isHTML(true);
 
-    /**
-     * To add the subject and the body of the mail.
-     */
+    //To add the subject and the body of the mail.
     $mail->Subject = "Auto Generated Mail.";
     $mail->Body = "Thank you for your submission.";
     
-    /**
-     * For sending the mail.
-     */
+    //For sending the mail.
     $mail->send();
   ?>
   
